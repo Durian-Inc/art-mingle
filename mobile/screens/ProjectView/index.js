305a1f627a5edeeb2e1690c05bcf9ce4a981b198
David Gardiner <dmgardiner25@gmail.com>
2019-11-09 09:13:05 -0600

    Create basic layout for project page

--- mobile/screens/ProjectView/index.js
+++ mobile/screens/ProjectView/index.js
@@ -8,14 +8,49 @@ import { Icon } from "react-native-eva-icons";
 
 const ProjectWrapper = styled.View`
   flex: 1;
+  margin: -60px -30px 0 -30px;
+`
+
+const IconWrapper = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  padding-right: 5px;
+`
+
+const ProjectHeader = styled.View`
+  width: 100%;
+  height: 50%;
+  padding: 60px 30px 0 30px;
+  background: #FFC28A;
+`
+
+const ProjectInfo = styled.View`
+  width: 100%;
+  height: 55%;
+  margin-top: -10%;
+  padding: 60px 30px 0 30px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  background: white;
 `
 
 const ProjectView = () => {
   return (
     <ProjectWrapper>
-      <Text h2>Hey Submission</Text>
+      <ProjectHeader>
+        <IconWrapper>
+          <Icon name="arrow-ios-back-outline" width={36} height={36}/>
+          <Icon name="heart-outline" width={36} height={36} />
+        </IconWrapper>
+        <View>
 
-      <Navigation />
+        </View>
+      </ProjectHeader>
+      <ProjectInfo>
+
+      </ProjectInfo>
     </ProjectWrapper>
   );
 };
