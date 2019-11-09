a95a526cdf33a8470d087b4cda1707b2a427fe7c
David Gardiner <dmgardiner25@gmail.com>
2019-11-09 03:44:56 -0600

    Create working navigation component

--- mobile/components/Navigation.js
+++ mobile/components/Navigation.js
@@ -1,35 +1,55 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import { Text, View, StyleSheet, Image } from "react-native";
+import { Link } from "react-router-native";
+import { Icon } from "react-native-eva-icons";
 
 const NavWrapper = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
   position: absolute;
+  bottom: 0;
   z-index: 1000;
+  padding: 10px 0;
   background: white;
 `;
 
-export default class Navigation extends Component {
-  render() {
-    return (
-      <NavWrapper>
-        <Link to="/" underlayColor="#f0f4f7" style={styles.navItem}>
+const NavItem = styled.View`
+  display: flex;
+  align-items: center;
+`
+
+const Navigation = () => {
+  return (
+    <NavWrapper>
+      <Link to="/" underlayColor="#f0f4f7" style={styles.navItem}>
+        <NavItem>
+          <Icon name="home-outline" width={20} height={20} fill="black" />
           <Text>Home</Text>
-        </Link>
-        <Link to="/groups" underlayColor="#f0f4f7" style={styles.navItem}>
+        </NavItem>
+      </Link>
+      <Link to="/groups" underlayColor="#f0f4f7" style={styles.navItem}>
+        <NavItem>
+          <Icon name="people-outline" width={20} height={20} fill="black" />
           <Text>Groups</Text>
-        </Link>
-        <Link to="/projects" underlayColor="#f0f4f7" style={styles.navItem}>
+        </NavItem>
+      </Link>
+      <Link to="/projects" underlayColor="#f0f4f7" style={styles.navItem}>
+        <NavItem>
+          <Icon name="clipboard-outline" width={20} height={20} fill="black" />
           <Text>Projects</Text>
-        </Link>
-        <Link to="/users" underlayColor="#f0f4f7" style={styles.navItem}>
+        </NavItem>
+      </Link>
+      <Link to="/users" underlayColor="#f0f4f7" style={styles.navItem}>
+        <NavItem>
+          <Icon name="search-outline" width={20} height={20} fill="black" />
           <Text>Users</Text>
-        </Link>
-        <Link to="/profile" underlayColor="#f0f4f7" style={styles.navItem}>
-          <Text>Profile</Text>
-        </Link>
-      </NavWrapper>
-    );
-  }
+        </NavItem>
+      </Link>
+    </NavWrapper>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -51,3 +71,5 @@ const styles = StyleSheet.create({
     width: 128
   }
 });
+
+export { Navigation }
--- mobile/screens/Home/index.js
+++ mobile/screens/Home/index.js
@@ -2,12 +2,20 @@ import React from "react";
 import { View } from "react-native";
 import { Link } from "react-router-native";
 import { Text } from "react-native-elements";
+import { Navigation } from "../../components/Navigation";
+import styled from "styled-components";
+
+const HomeWrapper = styled.View`
+  flex: 1;
+`
 
 const Home = () => {
   return (
-    <View>
+    <HomeWrapper>
       <Text h2>Hey Home</Text>
-    </View>
+
+      <Navigation />
+    </HomeWrapper>
   );
 };
 
