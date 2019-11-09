48f63ec8222ffc78f7e234bdc719425803e62fd4
Tom Dong <translucentleaf@protonmail.com>
2019-11-09 15:41:25 -0600

    Added web folder with React boilerplate

---
+++ web/src/components/home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const HomePage = styled.main`
+  background: blue;
+`;
+
+const Home = () => {
+  return (
+    <HomePage>
+      Homepage content bois!
+    </HomePage>
+  );
+}
