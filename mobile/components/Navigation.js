c4f112eae2f3839317d2bd4953d75a7725e6feb8
David Gardiner <dmgardiner25@gmail.com>
2019-11-10 03:24:20 -0600

    Make nav sizing platform specific

--- mobile/components/Navigation.js
+++ mobile/components/Navigation.js
@@ -1,19 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { Text, View } from "react-native";
+import { Text, View, Platform } from "react-native";
 import { Link } from "react-router-native";
 import { Icon } from "react-native-eva-icons";
 
 const NavWrapper = styled.View`
   display: flex;
   flex-direction: row;
-  height: 90px;
   flex: 1;
   position: absolute;
   bottom: 0;
   right: 0;
   left: 0;
-  padding-bottom: 30px;
   z-index: 1000;
   background: white;
   box-shadow: 0 4px 10px rgba(0,0,0,0.25)
@@ -32,8 +30,21 @@ const NavItem = styled(View)`
 `;
 
 const Navigation = () => {
+  const [platform, setPlatform] = useState("");
+
+  useEffect(() => {
+    if (platform === "") {
+      setPlatform(Platform.OS);
+    }
+  });
+
   return (
-    <NavWrapper>
+    <NavWrapper
+      style={{
+        paddingBottom: platform === "android" ? 0 : 30,
+        height: platform === "android" ? 65 : 90
+      }}
+    >
       <NavLink to="/" underlayColor="#f0f4f7">
         <NavItem>
           <Icon name="home-outline" width={20} height={20} fill="black" />
